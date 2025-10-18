a835242cf0daa48450ba297ca7eac5f907759aea
russell lee <149485763+scheriferen@users.noreply.github.com>
2025-10-18 23:30:50 +0800

    Added temporary navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,30 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Chat from "./components/ChatInterface/Chat";
+import Intro from "./components/IntroAnimation/Intro";
+import Land from "./components/LandingPage/LandingPage";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-        <Chat />
-    </div>
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Intro</Link>
+        <Link to="/chat">Chat</Link>
+        <Link to="/landing">Landing Page</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Intro />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/landing" element={<Land/>} />
+      </Routes>
+    </BrowserRouter>
+
+
+      
   );
 }
 
 export default App;
+
+/* if ur reading this no u didnt */
