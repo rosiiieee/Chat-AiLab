61070c238e0a6f704735d765bad17ceb322fe1a6
russell lee <149485763+scheriferen@users.noreply.github.com>
2025-10-23 02:40:30 +0800

    Updated App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,53 +1,59 @@
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
 import Chat from "./components/ChatInterface/Chat";
 import Intro from "./components/IntroAnimation/Intro";
 import Land from "./components/LandingPage/LandingPage";
-import Sidebar from "./components/Sidebar/Sidebar";
-import characterGif from "./draft_alab.gif";
+import Map from "./components/MapNavigation/Map";
+import Layout from "./components/Sidebar/Layout";
 import './App.css';
 
-function AppContent() {
-  const navigate = useNavigate();
-  const handleNavigation = (page) => {
-    switch(page) {
-      case 'home':
-        navigate('/landing');
-        break;
-      case 'chat':
-        navigate('/chat');
-        break;
-      case 'tour':
-        navigate('/map'); 
-        break;
-      case 'exit':
-        navigate('/landing');
-        break;
-      default:
-        navigate('/');
-    }
-  };
+function AnimatedRoutes() {
+  const location = useLocation();
+
   return (
-    <>
-      <Sidebar 
-        characterIcon={characterGif}
-        onNavigate={handleNavigation}
-      />
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Intro />} />
+        <Route path="/intro" element={<Intro />} />
+        
+
+        <Route element={<Layout />}>
+          <Route path="/landing" element={<Land />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/map" element={<Map />} />
+        </Route>
+      </Routes>
+    </AnimatePresence>
+  );
+}
+
+export default function App() {
+  return (
+    <Router>
+      <AnimatedRoutes />
+    </Router>
+  );
+}
+
+/* function App() {
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Intro</Link>
+        <Link to="/chat">Chat</Link>
+        <Link to="/landing">Landing Page</Link>
+      </nav>
+
       <Routes>
         <Route path="/" element={<Intro />} />
         <Route path="/chat" element={<Chat />} />
-        <Route path="/landing" element={<Land />} />
-        {/* Add MapNavigation route when ready */}
-        {/* <Route path="/map" element={<MapNavigation />} /> */}
+        <Route path="/landing" element={<Land/>} />
       </Routes>
-    </>
-  );
-}
-function App() {
-  return (
-    <BrowserRouter>
-      <AppContent />
+      
     </BrowserRouter>
   );
 }
-export default App;
-/* if ur reading this no u didnt */ 
+
+export default App; */
+
+/* if ur reading this no u didnt */
