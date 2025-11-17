e962b0129bd7a2edba66632f5fced14265ffde65
alex <122758355+alexong04@users.noreply.github.com>
2025-11-18 02:17:39 +0800

    switch to uuid to generate session IDs

--- frontend/src/components/Session.js
+++ frontend/src/components/Session.js
@@ -1,14 +1,14 @@
 import { useState, useEffect } from 'react'
+import { v4 as uuidv4 } from "uuid";
 
 export default function Session () {
   const [uuid, setUuid] = useState(() => {
-    return localStorage.getItem("uuid") || crypto.randomUUID();
+    return localStorage.getItem("uuid") || uuidv4();
   });
 
   useEffect(() => {
     localStorage.setItem("uuid", uuid);
   }, [uuid]);
 
-  console.log("Session ID:", uuid);
   return null;
 }
